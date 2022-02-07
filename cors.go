fe2b70db2ae3cf6d66b738ad2b4d54744f0b777e
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-02-07 19:44:06 +0530

    feat: Initial commit

---
+++ cors.go
@@ -0,0 +1,8 @@
+package cors
+
+import "github.com/gominima/minima"
+
+type cors struct {
+	res *minima.Response
+	req *minima.Request
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gominima/cors
+
+go 1.17
+
+require github.com/gominima/minima v0.1.2-alpha // indirect
