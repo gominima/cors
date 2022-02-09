4d0d04669a80675536a22c98225e8390e6f8d7fa
ToTu-Dev <75479355+ToTu-Dev@users.noreply.github.com>
2022-02-09 09:21:20 +0530

    feat: Done cors package

--- cors.go
+++ cors.go
@@ -1,8 +1,69 @@
 package cors
 
-import "github.com/gominima/minima"
+import (
+	"github.com/gominima/minima"
+	"github.com/rs/cors"
+	"net/http"
+)
 
-type cors struct {
-	res *minima.Response
-	req *minima.Request
+type Options = cors.Options
+
+/**
+@info The CorsWrapper for minima's instancr
+@property {*cors.Cors} [raw] The raw cors instance around which wrapper build
+@property {bool} [optionPassthrough] The bool value of options
+*/
+type corsWrapper struct {
+	*cors.Cors
+	optionPassthrough bool
+}
+
+/**
+@info Creates a corsWrapper instance
+@return {corsWrapper}
+*/
+func New() *corsWrapper {
+	return &corsWrapper{}
+}
+
+/**
+@info Builds the raw cors to minima handler
+@return {minima.Handler}
+*/
+func (c *corsWrapper) Build() minima.Handler {
+	return func(res *minima.Response, req *minima.Request) {
+		c.HandlerFunc(res.Raw(), req.Raw())
+		if !c.optionPassthrough && req.Method() == http.MethodOptions && req.Header().Get("Access-Control-Request-Method") != "" {
+			res.OK()
+			res.CloseConn()
+		}
+
+	}
+}
+
+/**
+@info Sets headers to allow all origins to make a request
+@return {minima.Handler}
+*/
+func (c *corsWrapper) AllowAll() minima.Handler {
+	crs := &corsWrapper{Cors: cors.AllowAll()}
+	return crs.Build()
+}
+
+/**
+@info Sets headers based on default cors setup
+@return {minima.Handler}
+*/
+func (c *corsWrapper) Default() minima.Handler {
+	crs := &corsWrapper{Cors: cors.Default()}
+	return crs.Build()
+}
+
+/**
+@info Sets headers based on the options given
+@return {minima.Handler}
+*/
+func (c *corsWrapper) NewCors(options Options) minima.Handler {
+	crs := &corsWrapper{Cors: cors.New(options), optionPassthrough: options.OptionsPassthrough}
+	return crs.Build()
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gominima/cors
 
 go 1.17
 
-require github.com/gominima/minima v0.1.2-alpha // indirect
+require (
+	github.com/gominima/minima v0.1.2-alpha.0.20220207142511-7e3d93afcb00 // indirect
+	github.com/rs/cors v1.8.2 // indirect
+)
